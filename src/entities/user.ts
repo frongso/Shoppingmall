240577d8c8120b636e8d1ec7d7590aed5400f57d
frongso <frong.so@hotmail.com>
2020-04-28 10:55:36 +0700

    change db

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,53 +1,29 @@
-import { Entity, Column, BaseEntity } from 'typeorm';
+import { BaseEntity, Column, Entity } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 
-@Entity({
-  name: 'msm_user',
-  schema: 'shopping_mall',
-})
+@Entity('msm_user', { schema: 'shopping_mall' })
 export class User extends BaseEntity {
   @Column('bigint', {
-    name: 'MSM_USER_ID',
     primary: true,
+    name: 'MSM_USER_ID',
     generated: true,
     width: 22,
   })
   id: number;
 
-  @Column('varchar', {
-    name: 'MSM_USR_USERNAME',
-    unique: true,
-    nullable: false,
-    length: 50,
-  })
+  @Column('varchar', { name: 'MSM_USERNAME', unique: true, nullable: false })
   username: string;
 
-  @Column('varchar', {
-    name: 'MSM_USR_PASSWORD',
-    nullable: false,
-    length: 250,
-  })
+  @Column('varchar', { name: 'MSM_PASSWORD', nullable: false })
   password: string;
 
-  @Column('varchar', {
-    name: 'MSM_USR_FIRSTNAME',
-    nullable: false,
-    length: 50,
-  })
-  firstname: string;
+  @Column('varchar', { name: 'MSM_USR_FIRST_NAME', nullable: false })
+  firstName: string;
 
-  @Column('varchar', {
-    name: 'MSM_USR_LASTNAME',
-    nullable: false,
-    length: 50,
-  })
-  lastname: string;
+  @Column('varchar', { name: 'MSM_USR_LAST_NAME', nullable: false })
+  lastName: string;
 
-  @Column('varchar', {
-    name: 'MSM_USR_EMAIL',
-    nullable: true,
-    length: 50,
-  })
+  @Column('varchar', { name: 'MSM_USR_EMAIL', nullable: true })
   email: string;
 
   hashPassword() {
@@ -58,3 +34,64 @@ export class User extends BaseEntity {
     return bcrypt.compareSync(unencryptedPassword, this.password);
   }
 }
+
+// import { Entity, Column, BaseEntity } from 'typeorm';
+// import * as bcrypt from 'bcryptjs';
+
+// @Entity({
+//   name: 'msm_user',
+//   schema: 'shopping_mall',
+// })
+// export class User extends BaseEntity {
+//   @Column('bigint', {
+//     name: 'MSM_USER_ID',
+//     primary: true,
+//     generated: true,
+//     width: 22,
+//   })
+//   id: number;
+
+//   @Column('varchar', {
+//     name: 'MSM_USR_USERNAME',
+//     unique: true,
+//     nullable: false,
+//     length: 50,
+//   })
+//   username: string;
+
+//   @Column('varchar', {
+//     name: 'MSM_USR_PASSWORD',
+//     nullable: false,
+//     length: 250,
+//   })
+//   password: string;
+
+//   @Column('varchar', {
+//     name: 'MSM_USR_FIRSTNAME',
+//     nullable: false,
+//     length: 50,
+//   })
+//   firstname: string;
+
+//   @Column('varchar', {
+//     name: 'MSM_USR_LASTNAME',
+//     nullable: false,
+//     length: 50,
+//   })
+//   lastname: string;
+
+//   @Column('varchar', {
+//     name: 'MSM_USR_EMAIL',
+//     nullable: true,
+//     length: 50,
+//   })
+//   email: string;
+
+//   hashPassword() {
+//     this.password = bcrypt.hashSync(this.password, 8);
+//   }
+
+//   checkIfUnencryptedPasswordIsValid(unencryptedPassword: string) {
+//     return bcrypt.compareSync(unencryptedPassword, this.password);
+//   }
+// }
